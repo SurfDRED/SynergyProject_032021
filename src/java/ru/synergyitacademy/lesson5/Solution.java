e5a5df97084627b18d63275c3b44a878a80c748e
Dmitii Iachmentsev <d.yachmentsev@gmail.com>
2021-04-18 14:57:07 +0200

    Корректировка решения по 5 уроку

--- src/java/ru/synergyitacademy/lesson5/Solution.java
+++ src/java/ru/synergyitacademy/lesson5/Solution.java
@@ -1,119 +1,129 @@
 package ru.synergyitacademy.lesson5;
 
+
 public class Solution {
 
     public static void main(String[] args){
         int number = 5; // умножение матрицы на число
         int degree = 3; // возведение в степень
-        Matrix matrixA = new Matrix();  //конструктор - по умолчанию - создается массив 2 x 2
-        int[][] userMatrixA = new int[2][2]; //массив 2 x 2 (квадратная матрица пользователя)
-        completionMatrix(matrixA.getUserMatrix());
-        completionMatrix(userMatrixA);
 
-        Matrix matrixB = new Matrix(3,3); //конструктор перегруженный - произвольная матрица m x n
-        int[][] userMatrixB = new int[3][3]; //массив m x n (пользователя)
-        completionMatrix(matrixB.getUserMatrix());
-        completionMatrix(userMatrixB);
-
-        //Матрицы 2х2
-        System.out.println("Размер матрицы А: " + matrixA.getM() + " х " + matrixA.getN());
-        matrixA.MatrixPrint(matrixA.getUserMatrix());
-        System.out.println("Размер матрицы В: " + userMatrixA.length + " х " + userMatrixA[0].length);
-        matrixA.MatrixPrint(userMatrixA);
+        //конструктор - по умолчанию - создается массив 2 x 2
+        Matrix mA1 = new Matrix();
+        Matrix mA2 = new Matrix();
+        //Заполняем матрицы 2 объектов рандомно
+        completionMatrix(mA1.getUserMatrix());
+        completionMatrix(mA2.getUserMatrix());
+        System.out.println("Размер матрицы А: " + mA1.getM() + " х " + mA1.getN());
+        mA1.matrixPrint();
+        System.out.println("Размер матрицы В: " + mA2.getM() + " х " + mA2.getN());
+        mA2.matrixPrint();
         System.out.println("--------------------------------------------------------");
         //1. Сложение матриц и вывод в консоль
         System.out.println("Сумма матриц А и В:");
-        matrixA.MatrixPrint(matrixA.MatrixAdd(userMatrixA));
+        Matrix summaA = mA1.matrixAdd(mA2);
+        summaA.matrixPrint();
         System.out.println("--------------------------------------------------------");
-        //2. Разница матриц и вывод в консоль
+        //2. Разность матриц и вывод в консоль
         System.out.println("Разность матриц А и В:");
-        matrixA.MatrixPrint(matrixA.MatrixDelete(userMatrixA));
+        Matrix deleteA = mA1.matrixDelete(mA2);
+        deleteA.matrixPrint();
         System.out.println("--------------------------------------------------------");
-        //3. Умножение матриц на число
+        //3. Умножение матрицы на число
         System.out.println("Умножение матрицы А на число: " + number);
-        matrixA.MatrixPrint(matrixA.MatrixMultiplicationNumber(number, matrixA.getUserMatrix()));
+        Matrix multiplicationNumberA1 = mA1.matrixMultiplicationNumber(number);
+        multiplicationNumberA1.matrixPrint();
         System.out.println("Умножение матрицы B на число: " + number);
-        matrixA.MatrixPrint(matrixA.MatrixMultiplicationNumber(number, userMatrixA));
+        Matrix multiplicationNumberA2 = mA2.matrixMultiplicationNumber(number);
+        multiplicationNumberA2.matrixPrint();
         System.out.println("--------------------------------------------------------");
         //4. Умножение матриц
         System.out.println("Умножение матрицы А на матрицу В:");
         try {
-            matrixA.MatrixPrint(matrixA.MatrixMultiplication(userMatrixA));
+            Matrix matrixMultiplicationA = mA1.matrixMultiplication(mA2);
+            matrixMultiplicationA.matrixPrint();
         } catch (Exception e) {
-            System.out.println("Ошибка! Число столбцов матрицы A:" + matrixA.getM() + " не равно числу строк матрицы B: " + userMatrixA[0].length);
+            System.out.println("Ошибка! Число столбцов матрицы A:" + mA1.getM() + " не равно числу строк матрицы B: " + mA2.getN());
         }
         System.out.println("--------------------------------------------------------");
         //5. Возведение матриц в степень
         System.out.println("Возведение в степень " + degree + " матрицы А:");
-        matrixA.MatrixPrint(matrixA.MatrixSquaring(degree, matrixA.getUserMatrix()));
+        Matrix matrixSquaringA1 = mA1.matrixSquaring(degree);
+        matrixSquaringA1.matrixPrint();
         System.out.println("Возведение в степень " + degree + " матрицы В:");
-        matrixA.MatrixPrint(matrixA.MatrixSquaring(degree, userMatrixA));
+        Matrix matrixSquaringA2 = mA2.matrixSquaring(degree);
+        matrixSquaringA2.matrixPrint();
         System.out.println("--------------------------------------------------------");
+        //6. Вычисление определителя 2х2
+        System.out.println("Вычисление определителя матрицы А - 2х2");
+        System.out.println(mA1.matrixDeterminant());
+        System.out.println("Вычисление определителя матрицы B - 2х2");
+        System.out.println(mA2.matrixDeterminant());
 
-        //Матрицы m х n
-        System.out.println("Размер матрицы А: " + matrixB.getM() + " х " + matrixB.getN());
-        matrixB.MatrixPrint(matrixB.getUserMatrix());
-        System.out.println("Размер матрицы В: " + userMatrixB.length + " х " + userMatrixB[0].length);
-        matrixB.MatrixPrint(userMatrixB);
+        //конструктор - по умолчанию - создается массив 2 x 2
+        Matrix mB1 = new Matrix();
+        Matrix mB2 = new Matrix();
+        //Заполняем матрицы 2 объектов рандомно
+        completionMatrix(mB1.getUserMatrix());
+        completionMatrix(mB2.getUserMatrix());
+        System.out.println("Размер матрицы А: " + mB1.getM() + " х " + mB1.getN());
+        mB1.matrixPrint();
+        System.out.println("Размер матрицы В: " + mB2.getM() + " х " + mB2.getN());
+        mB2.matrixPrint();
         System.out.println("--------------------------------------------------------");
         //1. Сложение матриц и вывод в консоль
         System.out.println("Сумма матриц А и В:");
-        matrixB.MatrixPrint(matrixB.MatrixAdd(userMatrixB));
+        Matrix summaB = mB1.matrixAdd(mB2);
+        summaB.matrixPrint();
         System.out.println("--------------------------------------------------------");
-        //2. Разница матриц и вывод в консоль
+        //2. Разность матриц и вывод в консоль
         System.out.println("Разность матриц А и В:");
-        matrixB.MatrixPrint(matrixB.MatrixDelete(userMatrixB));
+        Matrix deleteB = mB1.matrixDelete(mB2);
+        deleteB.matrixPrint();
         System.out.println("--------------------------------------------------------");
-        //3. Умножение матриц на число
+        //3. Умножение матрицы на число
         System.out.println("Умножение матрицы А на число: " + number);
-        matrixB.MatrixPrint(matrixB.MatrixMultiplicationNumber(number, matrixB.getUserMatrix()));
+        Matrix multiplicationNumberB1 = mB1.matrixMultiplicationNumber(number);
+        multiplicationNumberB1.matrixPrint();
         System.out.println("Умножение матрицы B на число: " + number);
-        matrixB.MatrixPrint(matrixB.MatrixMultiplicationNumber(number, userMatrixB));
+        Matrix multiplicationNumberB2 = mB2.matrixMultiplicationNumber(number);
+        multiplicationNumberB2.matrixPrint();
         System.out.println("--------------------------------------------------------");
         //4. Умножение матриц
         System.out.println("Умножение матрицы А на матрицу В:");
         try {
-            matrixB.MatrixPrint(matrixB.MatrixMultiplication(userMatrixB));
+            Matrix matrixMultiplicationB = mB1.matrixMultiplication(mB2);
+            matrixMultiplicationB.matrixPrint();
         } catch (Exception e) {
-            System.out.println("Ошибка! Число столбцов матрицы A:" + matrixB.getM() + " не равно числу строк матрицы B: " + userMatrixB[0].length);
+            System.out.println("Ошибка! Число столбцов матрицы A:" + mB1.getM() + " не равно числу строк матрицы B: " + mB2.getN());
         }
         System.out.println("--------------------------------------------------------");
         //5. Возведение матриц в степень
-            System.out.println("Возведение в степень " + degree + " матрицы А:");
-            matrixB.MatrixPrint(matrixB.MatrixSquaring(degree, matrixB.getUserMatrix()));
-            System.out.println("Возведение в степень " + degree + " матрицы В:");
-            matrixB.MatrixPrint(matrixB.MatrixSquaring(degree, userMatrixB));
+        System.out.println("Возведение в степень " + degree + " матрицы А:");
+        Matrix matrixSquaringB1 = mB1.matrixSquaring(degree);
+        matrixSquaringB1.matrixPrint();
+        System.out.println("Возведение в степень " + degree + " матрицы В:");
+        Matrix matrixSquaringB2 = mB2.matrixSquaring(degree);
+        matrixSquaringB2.matrixPrint();
         System.out.println("--------------------------------------------------------");
-        //6. Возврат единичной матрицы (через статичное поле и метод)
+        //6. Вычисление определителя 3х3
+        System.out.println("Вычисление определителя матрицы А - 3х3");
+        System.out.println(mB1.matrixDeterminant());
+        System.out.println("Вычисление определителя матрицы B - 3х3");
+        System.out.println(mB2.matrixDeterminant());
+        //7. Возврат единичной матрицы (через статичное поле и метод)
         System.out.println("Возврат единичной матрицы (через статичное поле и метод)");
-        matrixA.MatrixPrint(Matrix.getMatrixUnit());
+        Matrix matrixUnit = Matrix.getMatrixUnit();
+        matrixUnit.matrixPrint();
         System.out.println("--------------------------------------------------------");
-        //7. Вычисление определителя 2х2 или 3х3
-        System.out.println("Вычисление определителя матрицы А - 2х2");
-        System.out.println(matrixA.MatrixDeterminantA(matrixA.getUserMatrix()));
-        System.out.println("Вычисление определителя матрицы B - 2х2");
-        System.out.println(matrixA.MatrixDeterminantA(userMatrixA));
-        if (matrixB.getN() == 3 & matrixB.getM() == 3) {
-            System.out.println("Вычисление определителя матрицы А - 3х3");
-            System.out.println(matrixB.MatrixDeterminantB(matrixB.getUserMatrix()));
-        } else {
-            System.out.println("Размер матрицы не 3 х 3");
-        }
-        if (userMatrixB.length == 3 & userMatrixB[0].length == 3) {
-            System.out.println("Вычисление определителя матрицы B - 3х3");
-            System.out.println(matrixB.MatrixDeterminantB(userMatrixB));
-        } else {
-            System.out.println("Размер матрицы не 3 х 3");
-        }
     }
 
     //Рандомное заполнение матрицы от 0 до 10
-    public static int[][] completionMatrix(int[][] inputM){
-        for (int i = 0; i < inputM.length; i++) {
-            for (int j = 0; j < inputM[i].length; j++) {
-                inputM[i][j] = (int) (Math.random() * 10);
+    public static int[][] completionMatrix(int[][] input){
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                input[i][j] = (int) (Math.random() * 10);
             }
         }
-        return inputM;
+        return input;
     }
 }
